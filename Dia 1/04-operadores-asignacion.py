5fc17d794a2f6cf72a03bd9cc8de4579d54d7da6
pepelucho71922 <jose.gonzales.r@tecsup.edu.pe>
2021-05-17 20:07:55 -0500

    inicio, definicion de variables, impresiones, opreadores aritmericos y de asignación

---
+++ Dia 1/04-operadores-asignacion.py
@@ -0,0 +1,14 @@
+# OPERADORES DE ASIGNACIÓN
+# = igual
+x = 10
+# += incremento
+x += 1
+# -= decremento
+x -= 1
+# *= multiplicador
+x *= 2
+# /= dividendo
+x /=2
+# **= potencia
+x **=  2
+print(x)
