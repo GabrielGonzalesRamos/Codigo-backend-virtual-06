57b2f2bf9c8ba83206e4fb652347f817537e8933
pepelucho71922 <jose.gonzales.r@tecsup.edu.pe>
2021-05-24 19:00:30 -0500

    Rama semana 02

--- Dia 1/02-impresion-pantalla.py
+++
@@ -1,9 +0,0 @@
-estacion = "otoño"
-# concatenar varios valores, siempre y cuando todos sean STRING y pondrá un espacio entre valor y valor
-print("La estacion es:", estacion, ".")
-alumnos = 22
-grupo = 6
-print("Los alumnos del grupo {1} y son {0} alumnos  y son divertidos".format(alumnos,grupo))
-# la f funcionará como el metodo format usado anteriormente
-print(f"Los alumnos son en total {alumnos}")
-print(f"Los alumnos son en total {2+2}")
--- Dia 3/02-librerias.py
+++
@@ -1,5 +0,0 @@
-from camelcase import CamelCase
-# si quiero importar toda la clase ejecuto lo siguiente : "import camelcase"
-instancia = CamelCase()
-texto = "hola amigos"
-print(instancia.hump(texto))
