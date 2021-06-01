3c46a354b2e83bc2aaa102f80ffd89c7ef9f3880
pepelucho71922 <jose.gonzales.r@tecsup.edu.pe>
2021-05-31 22:03:21 -0500

    Dia 1

---
+++ Dia 1/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from dotenv import load_dotenv
+from os import environ
+app = Flask(__name__)
+# dialec
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URI")
+
+
+@app.route("/")
+def initial_controller():
+    return {
+        "message": "Welcome  API :cake:" 
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
