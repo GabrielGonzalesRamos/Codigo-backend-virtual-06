c166e15bfe8563a79403d9503f398898ec915b75
pepelucho71922 <jose.gonzales.r@tecsup.edu.pe>
2021-06-10 18:56:12 -0500

    Subir-Imagenes-con-UUID4

--- monedero/app.py
+++ monedero/app.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+import uuid
 from controllers.movimiento import MovimientosController
 from flask import Flask, app, request
 from dotenv import load_dotenv
@@ -11,6 +12,11 @@ from models.sesion import SesionModel
 from flask_jwt import JWT
 from config.seguridad import autenticador, identificador
 from config.custom_jwt import manejo_error_JWT
+# Para que en el nombre del archivo que manda el cliente antes de guardarlo, lo valide y evite que se guarde con caracteres
+# Especiales que puedan malograr el funcionamiento de la api o guardar de una forma incorrecta
+from werkzeug.utils import secure_filename
+from uuid import uuid4
+
 
 load_dotenv()
 
@@ -31,7 +37,7 @@ app.config['JWT_AUTH_USERNAME_KEY'] = 'correo'
 app.config['JWT_AUTH_PASSWORD_KEY'] = 'pass'
 # 1 byte * 1024 => 1 KB * 1024 => 1 MB * 1024 => 1 GB
 # MAX_CONTENT_LENGTH poner un limite  de maximo un 1MB de capacidad
-app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
 jsonwebtoken = JWT(app=app, authentication_handler=autenticador, identity_handler=identificador) # El "autenticador" hace referencia al 
 # archivo config.seguridad y la funcion autenticador de la clase Usuario 
 # Manejo de error
@@ -48,6 +54,9 @@ def archivos_permitidos(filename):
     # recibe dos parametros rsplit el primero es el caracteer a dividir y el segundo 
     # opccional es el que especifica en cuantas partes debe de ser dividido
     return '.' in filename and filename.rsplit('.', 1)[-1].lower() in EXTENSIONES_PERMITIDAS
+
+
+
 @app.route("/subirArchivo", methods=['POST'])
 def subir_archivo():
     print(request.files)
@@ -57,8 +66,19 @@ def subir_archivo():
     # Para saber el tipo de archivo 
     print(archivo.mimetype)
     if archivos_permitidos(archivo.filename):
-        archivo.save(path.join("multimedia", archivo.filename))
-        return 'Archivo Guardado Exitosamente'
+        # Primero saco el formato del archivo
+        formato_archivo = archivo.filename.rsplit(".",1)[-1]
+
+        # Genero un uuid y le agrego la extension
+        nombre_archivo = str(uuid4())+'.'+formato_archivo
+
+        # Acá validará que  no existan caracteres especiales que puedan romper el funcionamiento de mi api
+        archivo.filename = secure_filename(archivo.filename)
+        archivo.save(path.join("multimedia", nombre_archivo))
+        return {
+            "message": 'Archivo guardado exitosamente' ,
+            "success": True
+            }, 201
     return {
         "message": 'El archivo no esta permitido only %s' %(EXTENSIONES_PERMITIDAS),
         "success": False
