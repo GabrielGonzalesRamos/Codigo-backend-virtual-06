6cd053d63ee5e5dacc7109b3de34bac81a18dd4a
pepelucho71922 <jose.gonzales.r@tecsup.edu.pe>
2021-07-14 20:25:56 -0500

    Limpieza de la rama semana09

--- src/routes/tipo.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { crearTipo, listarTipo } from "../controllers/tipo";
-import { tipoRequestDto } from '../controllers/dto.request'
-
-export const tipoRouter: Router = Router();
-tipoRouter.route("/tipos").post(tipoRequestDto,crearTipo).get(listarTipo);
