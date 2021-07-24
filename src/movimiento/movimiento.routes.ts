64bc2dbb8c2c2449a2361fa8c3ed1f85815245df
pepelucho71922 <jose.gonzales.r@tecsup.edu.pe>
2021-07-23 21:40:00 -0500

    Se añadió el controaldor de listar de movimientos

--- src/movimiento/movimiento.routes.ts
+++ src/movimiento/movimiento.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { authValidator, personalValidador } from "../utils/validador";
-import { crearMovimiento, crearPreferencia, mpEventos } from './movimiento.controllers';
+import { crearMovimiento, crearPreferencia, mpEventos, listarMovimientos } from './movimiento.controllers';
 
 export const movimientoRouter = Router();
 
-movimientoRouter.route('/movimiento').post(authValidator, crearMovimiento);
+movimientoRouter.route('/movimiento').post(authValidator, crearMovimiento).get(authValidator, listarMovimientos);
 movimientoRouter.route('/venta').post(authValidator, personalValidador, crearPreferencia);
 movimientoRouter.route('/mercadopago-ipn').post(mpEventos);
